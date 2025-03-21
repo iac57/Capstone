0cf5d68e6b79ebabd50a4a64e7e8198c9b1c6726
Evan Sharafuddin <evansharafuddin@wustl.edu>
2025-03-20 22:25:50 -0500

    some more changes, stuff isnt working a ton

--- evan_scripts/dynamics.m
+++ evan_scripts/dynamics.m
@@ -4,6 +4,20 @@
 % Evan Sharafuddin, Izzy Collins, Gabe Garcia
 % 3/19/2025
 
+%%% References
+% https://cpjobling.github.io/eglm03-textbook/intro.html
+% Dr. Wise, ESE 543, WashU
+% https://ctms.engin.umich.edu/CTMS/index.php?example=Introduction&section=ControlRootLocus
+% T. Wong, “Design of a magnetic levitation control system- an under- graduate problem,” IEEE Transactions on Education, vol. E-29, no. 4, pp. 196–200, 1986
+
+%%% NOTES
+% I do not know what is going on. ss2tf and transfer function derivations
+% are yielding different results. Need to determine which one is actually
+% right
+
+
+
+
 clear
 clc
 close all
@@ -27,60 +41,88 @@ x0 = x0 / 1e3; % [mm] -> [m]
 i0 = sqrt( m*g*x0^2 / K ); % A
 
 %% STEP 1: Determine governing differential equation for the MagLev system
-% represent as a system of first-order ODEs
-dx_dt = @(t, x) [x(2)                ; 
-                 -K/m * (i0/x(1))^2 + g]; % NOTE sign error in overleaf fixed here
-tspan = [0 1];
+% % represent as a system of first-order ODEs
+% dx_dt = @(t, x) [x(2)                ; 
+%                  -K/m * (i0/x(1))^2 + g]; % NOTE sign error in overleaf fixed here
+% tspan = [0 1];
+% 
+% % start at X, no initial velocity
+% [t, y] = ode45(dx_dt, tspan, [x0 0]);
+% figure, plot(t, y(:,1))
+% 
+% % start at X, small initial velocity away from magnet
+% [t, y] = ode45(dx_dt, tspan, [x0 0.00001]);
+% figure, plot(t, y(:,1))
+% 
+% % start at X, small initial velocity toward magnet
+% [t, y] = ode45(dx_dt, tspan, [x0 -0.01]);
+% figure, plot(t, y(:,1))
+% 
+% % start at small distance from X away from magnet
+% [t, y] = ode45(dx_dt, tspan, [x0+0.0001 0]);
+% figure, plot(t, y(:,1))
+% 
+% % start at small distance from X towards magnet
+% [t, y] = ode45(dx_dt, tspan, [x0-0.0001 0]);
+% figure, plot(t, y(:,1))
 
-% start at X, no initial velocity
-[t, y] = ode45(dx_dt, tspan, [x0 0]);
-figure, plot(t, y(:,1))
-
-% start at X, small initial velocity away from magnet
-[t, y] = ode45(dx_dt, tspan, [x0 0.00001]);
-figure, plot(t, y(:,1))
-
-% start at X, small initial velocity toward magnet
-[t, y] = ode45(dx_dt, tspan, [x0 -0.01]);
-figure, plot(t, y(:,1))
-
-% start at small distance from X away from magnet
-[t, y] = ode45(dx_dt, tspan, [x0+0.0001 0]);
-figure, plot(t, y(:,1))
-
-% start at small distance from X towards magnet
-[t, y] = ode45(dx_dt, tspan, [x0-0.0001 0]);
-figure, plot(t, y(:,1))
 
 %% STEP 2: Hand tune a PID controller
 % Create plant and controller transfer functions
 s = tf('s');
 
 Kp = 100;
-Ki = 100;
-Kd = 100;
+Ki = 10000;
+Kd = 5;
 
 a_21 = 2*K*i0^2 / (m*x0^3);
 b_2 = 2*K*i0 / (m*x0^2);
+% a_21 = 1;
+% b_2 = 1;
 
-P = b_2 / ( s*(s-a_21) ); % plant
+P = b_2 / ( s^2 + a_21 );
 Kt = Kp + Ki/s + Kd*s; % controller
+% 
+% % simulate open loop response at rest
+% % t = linspace(0, 1, 1e3);
+% % y = lsim(P, zeros(size(t)), t);
+% % figure, plot(t,y)
+% % title("Open loop response")
+% 
+% % simulate impulse imput
+% u = zeros(size(t));
+% u(1) = 1;
+% y = lsim(P,u,t);
+% figure, plot(t,y)
+% 
+% % add control
+% cl = minreal((P*Kt)/(1+P*Kt));
+% u = ones(size(t)) * 0.005; % go 5 mm away from equilib
+% y = lsim(cl, u, t);
+% figure, plot(t, y)
+% title("Controlled output")
+% 
+% %%%Analyse the open loop system
+% sys_OL = P*Kt;
+% figure, margin(sys_OL)
 
-% simulate open loop response at rest
-t = linspace(0, 1, 1e3);
-y = lsim(P, zeros(size(t)), t);
-figure, plot(t,y)
+%% STEP: state space representation
+% a_21 = 1;
+% b_2 = 1;
 
-% simulate impulse imput
-u = zeros(size(t));
-u(1) = 1;
-y = lsim(P,u,t);
-figure, plot(t,y)
+A = [0    1;
+     a_21 0];
+B = [0; b_2];
+C = [1 0];
+D = 0;
 
-% add control
-cl = minreal((P*Kt)/(1+P*Kt));
-u = ones(size(t)) * 0.005; % go 5 mm away from equilib
-y = lsim(cl, u, t);
-figure, plot(t, y)
 
-%% STEP 3: Simulate noise, sensor bandwidth, and filtering
+[b, a] = ss2tf(A, B, C, D);
+sys = tf(b, a);
+rlocus(sys)
+step(sys)
+
+
+
+
+%% STEP 4: Simulate noise, sensor bandwidth, and filtering
